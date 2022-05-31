4a767f3f24fff30c49876ce262bdd76ad6979781
omer <peynirciomerfaruk@gmail.com>
2022-05-31 11:15:52 +0200

    books list display in

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { add, remove, oneMore, oneLess } from "../cartSlice";
+import { data } from "../data";
 
 const Products = (props) => {
   const dispatch = useDispatch();
@@ -11,34 +12,30 @@ const Products = (props) => {
         <span>Kitap Listesi</span>
         <Link to="/cart">Sepetim</Link>
       </h2>
-      <div className="book">
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg"
-          alt="Simyaci"
-        />
-        <div>
-          <h4>Simyaci</h4>
-          <p>Yazar: Paulo Coelho</p>
-          <p>Fiyat: &#8378; 19.99</p>
-          <button
-            onClick={() => {
-              dispatch(
-                add({
-                  id: 1,
-                  name: "Simyacı",
-                  author: "Paulo Coelho",
-                  price: 9.99,
-                  image:
-                    "https://images-na.ssl-images-amazon.com/images/I/51eqjXwFzwL._SX344_BO1,204,203,200_.jpg",
-                  qty: 1,
-                })
-              );
-            }}
-          >
-            Sepete Ekle
-          </button>
-        </div>
-      </div>
+      {data.map((book, index) => {
+        return (
+          <div className="book" key={index}>
+            <img src={book.image} alt={book.name} />
+            <div>
+              <h4>{book.name}</h4>
+              <p>Yazar: {book.author}</p>
+              <p>Fiyat: &#8378; {book.price}</p>
+              <button
+                onClick={() => {
+                  dispatch(
+                    add({
+                      ...book,
+                      qty: 1,
+                    })
+                  );
+                }}
+              >
+                Sepete Ekle
+              </button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
